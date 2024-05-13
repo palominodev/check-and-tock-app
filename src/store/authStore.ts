c759c02ce88c46b8ba754c041c9c50dff4ab6b0b
palominodev <jeremypalomino.dev@gmail.com>
2024-05-13 18:38:34 -0500

    Creación del Store

---
+++ src/store/authStore.ts
@@ -0,0 +1,14 @@
+import { create } from 'zustand'
+
+type State = {
+	isLogged: boolean;
+	onLogin: () => void
+	onLogout: () => void
+}
+
+export const useAuthStore = create<State>((set) => ({
+	isLogged: false,
+	onLogin: () => set(() => ({isLogged: true})),
+	onLogout: () => set(() => ({isLogged: false})),
+}))
+
