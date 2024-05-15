455be7ec15c913b217e5e6fee30d0e23edfac532
palominodev <jeremypalomino.dev@gmail.com>
2024-05-15 18:56:56 -0500

    Formulario para agregar productos

---
+++ src/firebase/service/addProduct.ts
@@ -0,0 +1,18 @@
+import { addDoc, collection, doc } from "firebase/firestore"
+import { FirebaseDB } from "../config"
+
+export type Product = {
+	name: string
+	price: string
+	sede: string
+}
+export const addProduct = async(product: Product) =>{
+	const sedeRef = doc(FirebaseDB, 'sede', product.sede)
+	await addDoc(collection(FirebaseDB, "producto"),{
+		...product,
+		price: Number(product.price),
+		sede: sedeRef
+	});
+	
+	
+}
--- src/pages/dashboard/products/CrearProductoForm.tsx
+++ src/pages/dashboard/products/CrearProductoForm.tsx
@@ -1,6 +1,29 @@
+import { useEffect } from "react";
+import { Product, addProduct } from "../../../firebase/service/addProduct";
+import { useDataStore } from "../../../store/dataStore";
+import { getSedes } from "../../../firebase/service/getSedes";
+
 export const CrearProductoForm = () => {
+	const sedes = useDataStore(state => state.sede)
+	const setSedes = useDataStore(state => state.setSedes)
+	const onCreateProduct = async(e:any) => {
+		e.preventDefault()
+		const {name_product, sede_product, price_product} = Object.fromEntries(new FormData(e.target))		
+		await addProduct({
+			name: name_product,
+			sede: sede_product,
+			price: price_product
+		} as Product)
+	}
+
+	useEffect(() => {
+		(async() => {
+			const sedes = await getSedes()
+			setSedes(sedes)
+		})()
+	},[])
   return (
-	<form>
+	<form onSubmit={onCreateProduct}>
 		<h1 className="text-4xl font-bold mb-5">Crear productos</h1>
 		<label className="block mb-3">
 			<p className="text-lg font-bold">Nombre del producto</p>
@@ -8,14 +31,19 @@ export const CrearProductoForm = () => {
 		</label>
 		<label className="block mb-3">
 			<p className="text-lg font-bold">Sede</p>
-			<select className="border p-2 text-lg" name="sede_product" id="sede_product">
-				<option value="Villa el Salvador">Villa el Salvador</option>
-				<option value="Hospital">Hospital</option>
+			<select disabled={!sedes} className="border p-2 text-lg" name="sede_product" id="sede_product">
+				{
+					sedes.length === 0
+					? <option>Cargando Sedes..</option>
+					: sedes.map((sede:any) => (
+						<option value={sede.id} key={sede.id}>{sede.nombre}</option>
+					))
+				}
 			</select>
 		</label>
 		<label className="block mb-3">
 			<p className="text-lg font-bold">Precio</p>
-			<input className="border p-2 text-lg" type="number" name="price_product" id="price_product" />
+			<input step="any" className="border p-2 text-lg" type="number" name="price_product" id="price_product" />
 		</label>
 		<input className="bg-green-600 px-5 py-3 font-bold text-white rounded-lg" type="submit" value="Agregar" />
 	</form>
