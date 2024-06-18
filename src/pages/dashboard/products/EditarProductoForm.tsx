d6595fc5d92da7a1f862419b4fd5885fa1b54f94
palominodev <jeremypalomino.dev@gmail.com>
2024-06-18 15:06:47 -0500

    Actualización a Shadcn ui

--- src/pages/dashboard/products/EditarProductoForm.tsx
+++ src/pages/dashboard/products/EditarProductoForm.tsx
@@ -1,23 +1,34 @@
+import { CategoriaSelect } from "@/components/CategoriaSelect"
+import { SedesSelect } from "@/components/SedesSelect"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 export const EditarProductoForm = () => {
   return (
 	<form>
 		<h1 className="text-4xl font-bold mb-5">Editar productos</h1>
-		<label className="block mb-3">
+				<Label className="block mb-3">
 			<p className="text-lg font-bold">Nombre del producto</p>
-			<input className="border p-2 text-lg" type="text" name="name_product" id="name_product" />
-		</label>
-		<label className="block mb-3">
+			<Input className="border p-2 text-lg" type="text" name="name_product" id="name_product" />
+		</Label>
+		<Label className="block mb-3">
 			<p className="text-lg font-bold">Sede</p>
-			<select className="border p-2 text-lg" name="sede_product" id="sede_product">
-				<option value="Villa el Salvador">Villa el Salvador</option>
-				<option value="Hospital">Hospital</option>
-			</select>
-		</label>
-		<label className="block mb-3">
+			<SedesSelect />
+		</Label>
+		<Label className="block mb-3">
+			<p className="text-lg font-bold">Categoria</p>
+			<CategoriaSelect />
+		</Label>
+		<Label className="block mb-3">
 			<p className="text-lg font-bold">Precio</p>
-			<input className="border p-2 text-lg" type="number" name="price_product" id="price_product" />
-		</label>
-		<input className="bg-green-600 px-5 py-3 font-bold text-white rounded-lg" type="submit" value="Agregar" />
+			<Input step="any" className="border p-2 text-lg" type="number" name="price_product" id="price_product" />
+		</Label>
+		<Label className="block mb-3">
+			<p className="text-lg font-bold">Cantidad mínima</p>
+			<Input step="any" className="border p-2 text-lg" type="number" name="cantidad_minima" id="cantidad_minima" />
+		</Label>
+		<Button className="bg-green-600 px-5 py-3 font-bold text-white rounded-lg" type="submit">Agregar</Button>
 	</form>
   )
 }
